9cbd13f5a87df1363c35677c66c29487b701304e
R0D7 <121826354+R0D7@users.noreply.github.com>
2023-03-07 13:38:44 +0100

    Update get_balance_of_ethereum.py

--- get_balance_of_ethereum.py
+++ get_balance_of_ethereum.py
@@ -1,8 +1,8 @@
 from web3 import Web3
 
 def get_balance_of_eth(wallet_eth):
-    # Insert your Ethereum node provider url instead of "node provider url"
-    web3 = Web3(Web3.HTTPProvider(ethereum_node_provider_url))
+    # Insert your Ethereum node provider url instead of "ethereum_node_provider_url"
+    web3 = Web3(Web3.HTTPProvider("ethereum_node_provider_url"))
     # Get the balance of the wallet 
     balance_wei = float((web3.eth.get_balance(wallet_eth)))
     # Convert the balance (wei) in ether
