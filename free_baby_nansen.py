714c3c588e3f4926ded10d43636a5d54bcbd4bd0
R0D7 <121826354+R0D7@users.noreply.github.com>
2023-03-07 13:39:04 +0100

    Update free_baby_nansen.py

--- free_baby_nansen.py
+++ free_baby_nansen.py
@@ -7,7 +7,6 @@ while i is True :
     eth_wallet = input("Enter an ethereum adress : ")
     #Verify if the adress format is the address formats is recognize
     if Web3.isAddress(eth_wallet) is True :
-
         print("The wallet has", get_balance_of_eth(eth_wallet)*get_eth_price_(), "$ of Ethereum")
         print("----------------------------------------")
     else : print("Invalid adress")
